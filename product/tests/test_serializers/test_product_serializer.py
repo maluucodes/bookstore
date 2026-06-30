a38f1e2fa41ab088154307ca33f09251c34fca46
Maria Luiza <m.luiza0020@gmail.com>
2026-06-30 12:10:08 -0300

    fixing serializer tests

--- product/tests/test_serializers/test_product_serializer.py
+++ product/tests/test_serializers/test_product_serializer.py
@@ -1,26 +1,7 @@
 from django.test import TestCase
 
 from product.models import Category, Product
-from product.serializers.category_serializer import CategorySerializer
 from product.serializers.product_serializer import ProductSerializer
-
-
-class CategorySerializerTest(TestCase):
-
-    def test_category_serializer(self):
-        category = Category.objects.create(
-            title="Livros",
-            slug="livros",
-            description="Categoria de livros",
-            active=True
-        )
-
-        serializer = CategorySerializer(category)
-
-        self.assertEqual(serializer.data["title"], "Livros")
-        self.assertEqual(serializer.data["slug"], "livros")
-        self.assertEqual(serializer.data["description"], "Categoria de livros")
-        self.assertEqual(serializer.data["active"], True)
 
 
 class ProductSerializerTest(TestCase):
